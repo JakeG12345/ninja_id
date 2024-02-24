b634189574332123b5c201f0a56b1e392836ea75
Jake Glick <jake.glick@student.moriah.nsw.edu.au>
2024-02-24 18:30:47 +1100

    Complete tutorial 15 - make UI for home page - appbar, pfp, name, ninja level, email

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: NinjaCard()
-));
+void main() => runApp(MaterialApp(home: NinjaCard()));
 
 class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ninja ID Card')
-      ),
-    );
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text(
+            'Ninja ID Card',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+        ),
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/thumb.jpg'),
+                    radius: 40,
+                  ),
+                ),
+                Divider(
+                  height: 90,
+                  color: Colors.grey[800],
+                ),
+                Text('NAME',
+                    style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+                Text('Chun-Li',
+                    style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 30,
+                ),
+                Text('CURRENT NINJA LEVEL',
+                    style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+                Text('8',
+                    style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.grey[400]),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'chun.li@thenetninja.co.uk',
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 18.0,
+                          letterSpacing: 1.0),
+                    )
+                  ],
+                )
+              ],
+            )));
   }
 }
